96aa58fcce313fc40892212ecfffcca5d9fd865c
ynsz <99311398+ynsz@users.noreply.github.com>
2025-09-17 22:09:03 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Sidebar from "./components/sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,67 @@
+import "./Sidebar.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
+import SidebarChannel from "./SidebarChannel";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* SidebaLeft */}
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="/vite.svg" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="/vite.svg" alt="" />
+        </div>
+      </div>
+      {/* SidebarRight */}
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+
+        {/* SidebarChannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>ShinCode</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
