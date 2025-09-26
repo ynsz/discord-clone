373188e1c81dcd31dd421e5ea9871b34b6749b3b
ynsz <99311398+ynsz@users.noreply.github.com>
2025-09-26 18:15:11 +0900

    ログイン機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import Chat from "./components/chat/Chat";
 import Sidebar from "./components/sidebar/Sidebar";
 import Login from "./components/login/Login";
 import { useAppSelecter } from "./app/hocks";
+import { useEffect } from "react";
 
 function App() {
   const user = useAppSelecter((state) => state.user);
   // const user = null;
   console.log(user);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
       {user ? (
