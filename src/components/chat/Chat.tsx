eba1d1e0a8892113b363c3dae1d84a81386b00d8
ynsz <99311398+ynsz@users.noreply.github.com>
2025-10-09 15:15:33 +0900

    入力文字列の取得

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -8,9 +8,15 @@ import "./Chat.scss";
 import { ChatHeader } from "./ChatHeader";
 import ChatMessage from "./ChatMessage";
 import { useAppSelecter } from "../../app/hocks";
+import { useState } from "react";
 
 function Chat() {
+  const [inputText, setInputText] = useState<string>("");
   const channelName = useAppSelecter((state) => state.channel.channelName);
+
+  const sendMessage = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="chat">
@@ -27,8 +33,20 @@ function Chat() {
       <div className="chatInput">
         <AddCircleOutline />
         <form>
-          <input type="text" placeholder="#Udemyへメッセージを送信" />
-          <button type="submit" className="chatInputButton">
+          <input
+            type="text"
+            placeholder="#Udemyへメッセージを送信"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            className="chatInputButton"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              sendMessage(e)
+            }
+          >
             送信
           </button>
         </form>
